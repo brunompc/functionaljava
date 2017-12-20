00f51e29d718783b39e5587d6d98276f97a7836f
bmpc <bcipriano@ulusofona.pt>
2017-12-20 11:45:17 +0000

    Added example of mapping with stream().

--- src/functionaljava/FunctionalJava.java
+++ src/functionaljava/FunctionalJava.java
@@ -4,8 +4,10 @@ package functionaljava;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
+ * Some experiments with Java 8's Functional Programming constructs.
  * 
  * @author BC
  */
@@ -26,8 +28,14 @@ public class FunctionalJava {
         numbers.replaceAll(n -> n * factor);
     }
     
+    // Non-Destructive (using Stream) "mapping" operation
+    static List<Integer> divideBy(List<Integer> numbers, int d) {
+        return numbers.stream().map(n -> n / d).collect(Collectors.toList());
+    }
+    
     public static void main(String[] args) {
 
+        // multiplyBy
         List<Integer> numbers = new ArrayList<>();
         numbers.add(1);
         numbers.add(33);
@@ -36,6 +44,7 @@ public class FunctionalJava {
         multiplyBy(numbers, 3);
         System.out.println("After:" + Arrays.toString(numbers.toArray()));
         
+        // removeNegatives
         List<Integer> moreNumbers = new ArrayList<>();
         for(int i=-3; i<=3; i++) {
             moreNumbers.add(i);
@@ -44,14 +53,24 @@ public class FunctionalJava {
         removeNegatives(moreNumbers);
         System.out.println("After:" + Arrays.toString(moreNumbers.toArray()));
         
+        // addSuffix
         List<String> words = new ArrayList<>();
         words.add("Peace");
         words.add("Love");
         words.add("Music");
-        
         System.out.println("Before:" + Arrays.toString(words.toArray()));
         addSuffix(words, " Lover");
         System.out.println("After:" + Arrays.toString(words.toArray()));
+        
+        // divideBy
+        List<Integer> numbers1 = new ArrayList<>();
+        numbers1.add(10);
+        numbers1.add(20);
+        numbers1.add(30);
+        List<Integer> numbers2 = divideBy(numbers1, 2);
+        System.out.println("Before:" + Arrays.toString(numbers1.toArray()));
+        addSuffix(words, " Lover");
+        System.out.println("After:" + Arrays.toString(numbers2.toArray()));
         
     }
     
